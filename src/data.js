dc958bf882cda3d2b6a954249595b7fcb94284ac
Maxime Leriche <maxleriche.60@gmail.com>
2021-10-14 17:07:26 +0200

    Init project

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export default {
+  user: {
+    id: 4,
+    username: "CrashDummy",
+    name: "Jessica johnes",
+    ico: "",
+    score: "NaN",
+  },
+  leaderBoard: [
+    {
+      id: 3,
+      name: "User1",
+      score: 90000,
+    },
+    {
+      id: 2,
+      name: "User2",
+      score: 90000,
+    },
+    {
+      id: 1,
+      name: "User3",
+      score: 90000,
+    },
+  ],
+};
